e270abc6e3efce66b5263a4957da3e8734b52e11
StefanIulianGrigoriu <grigoriu.stefan.iulian@gmail.com>
2023-03-07 14:29:32 +0000

    add pseudo line chart

--- src/App.js
+++ src/App.js
@@ -8,24 +8,58 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello SVG</h1>
-      <svg width="598" height="388" fill="steelblue">
-        <rect width="598" height="388" fill="white" />
-        <rect x="106" y="30" width="120" height="47" fill="black" />
-        <rect x="106" y="306" width="185" height="47" fill="black" />
-        <rect x="106" y="214" width="463" height="47" fill="black" />
-        <rect x="106" y="122" width="277" height="47" fill="black" />
-        <text x="40" y="60" fill="black">
-          ABC
-        </text>
-        <text x="40" y="155" fill="black">
-          DEF
-        </text>
-        <text x="40" y="245" fill="black">
-          GHI
-        </text>
-        <text x="40" y="340" fill="black">
-          JKL
-        </text>
+      <svg width="608" height="408" fill="none">
+        <g filter="url(#filter0_d_0_3)">
+          <rect
+            width="600"
+            height="400"
+            transform="translate(4)"
+            fill="white"
+          />
+          <path
+            d="M66 372L155.405 155.201L225 304L286 212L359.5 372L436 63.5L496 372"
+            stroke="black"
+            stroke-width="20"
+            stroke-linejoin="round"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_d_0_3"
+            x="0"
+            y="0"
+            width="608"
+            height="408"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_0_3"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_0_3"
+              result="shape"
+            />
+          </filter>
+        </defs>
       </svg>
     </div>
   );
--- src/components/Fundamentals.jsx
+++ src/components/Fundamentals.jsx
@@ -29,13 +29,34 @@ export default function Fundamentals() {
         <rect x="458" y="51" width="63" height="283" />
       </svg> */}
 
-      {/* Bar Chart example      
+      {/* Horizontal Bar Chart example      
       <svg width="598" height="388" fill="steelblue">
         <rect width="598" height="388" fill="white" />
         <rect x="31" y="30" width="127" height="48" />
         <rect x="31" y="309" width="195" height="48" />
         <rect x="31" y="216" width="488" height="48" />
         <rect x="31" y="123" width="292" height="48" />
+      </svg> */}
+
+      {/* Horizontal Bar Chart example with labels
+      <svg width="598" height="388" fill="steelblue">
+        <rect width="598" height="388" fill="white" />
+        <rect x="106" y="30" width="120" height="47" fill="black" />
+        <rect x="106" y="306" width="185" height="47" fill="black" />
+        <rect x="106" y="214" width="463" height="47" fill="black" />
+        <rect x="106" y="122" width="277" height="47" fill="black" />
+        <text x="40" y="60" fill="black">
+          ABC
+        </text>
+        <text x="40" y="155" fill="black">
+          DEF
+        </text>
+        <text x="40" y="245" fill="black">
+          GHI
+        </text>
+        <text x="40" y="340" fill="black">
+          JKL
+        </text>
       </svg> */}
       <svg width="500" height={svgHeight}>
         <rect
