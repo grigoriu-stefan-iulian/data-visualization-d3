538e36904af4d23f6ca39d58d6865aba4de6553a
StefanIulianGrigoriu <grigoriu.stefan.iulian@gmail.com>
2023-03-08 14:58:13 +0000

    d3 symbols

--- src/App.js
+++ src/App.js
@@ -1,66 +1,64 @@
+import { select, range, symbols, symbol } from "d3";
+
 import "./styles.css";
 
-export default function App() {
-  const svgWidth = 500;
-  const svgHeight = 250;
-  const strokeWidth = 20;
+const svgWidth = window.innerWidth;
+const svgHeight = window.innerHeight;
+const strokeWidth = 20;
 
+console.log(symbol);
+
+const numberOfRectangles = 25;
+
+const svg = select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+svg
+  .append("g")
+  .selectAll("rect")
+  .data(range(numberOfRectangles))
+  .join("rect")
+  .attr("y", (d) => d * 20)
+  .attr("width", svgWidth)
+  .attr("height", 10)
+  .attr("mask", "url(#mask1)");
+
+svg
+  .append("g")
+  .selectAll("rect")
+  .data(range(numberOfRectangles))
+  .join("rect")
+  .attr("x", (d) => d * 20)
+  .attr("width", 10)
+  .attr("height", svgHeight)
+  .attr("mask", "url(#mask2)");
+
+const renderMask = (maskName, maskFill1, maskFill2) => {
+  const mask = svg.append("mask").attr("id", maskName);
+
+  mask
+    .append("rect")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr("fill", maskFill1);
+
+  mask
+    .append("g")
+    .attr("transform", `translate(${svgWidth / 6}, ${svgHeight / 4})`)
+    .append("path")
+    .attr("d", symbol(symbols[2], 20000)())
+    .attr("fill", maskFill2);
+};
+
+renderMask("mask1", "black", "white");
+renderMask("mask2", "white", "black");
+
+export default function App() {
   return (
     <div className="App">
       <h1>Hello SVG</h1>
-      <svg width="608" height="408" fill="none">
-        <g filter="url(#filter0_d_0_3)">
-          <rect
-            width="600"
-            height="400"
-            transform="translate(4)"
-            fill="white"
-          />
-          <path
-            d="M66 372L155.405 155.201L225 304L286 212L359.5 372L436 63.5L496 372"
-            stroke="black"
-            stroke-width="20"
-            stroke-linejoin="round"
-          />
-        </g>
-        <defs>
-          <filter
-            id="filter0_d_0_3"
-            x="0"
-            y="0"
-            width="608"
-            height="408"
-            filterUnits="userSpaceOnUse"
-            color-interpolation-filters="sRGB"
-          >
-            <feFlood flood-opacity="0" result="BackgroundImageFix" />
-            <feColorMatrix
-              in="SourceAlpha"
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              result="hardAlpha"
-            />
-            <feOffset dy="4" />
-            <feGaussianBlur stdDeviation="2" />
-            <feComposite in2="hardAlpha" operator="out" />
-            <feColorMatrix
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-            />
-            <feBlend
-              mode="normal"
-              in2="BackgroundImageFix"
-              result="effect1_dropShadow_0_3"
-            />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="effect1_dropShadow_0_3"
-              result="shape"
-            />
-          </filter>
-        </defs>
-      </svg>
     </div>
   );
 }
--- src/components/Fundamentals.jsx
+++ src/components/Fundamentals.jsx
@@ -5,6 +5,97 @@ export default function Fundamentals() {
   const svgHeight = 250;
   const strokeWidth = 20;
 
+  // Lewit Art with D3 and pure JS intro
+  // The visual marks that will appear on the page
+  // const marks = [];
+
+  // for (let i = 0; i < 25; i++) {
+  //   marks.push({
+  //     y: i * 20,
+  //     width: svgWidth,
+  //     height: 10,
+  //     mask: "url(#circle-mask)",
+  //   });
+  // }
+
+  // const svg = select("body")
+  //   .append("svg")
+  //   .attr("width", svgWidth)
+  //   .attr("height", svgHeight);
+
+  // svg
+  //   .selectAll("rect")
+  //   .data(marks)
+  //   .join("rect")
+  //   .attr("y", (data) => data.y)
+  //   .attr("width", (data) => data.width)
+  //   .attr("height", (data) => data.height)
+  //   .attr("mask", (data) => data.mask);
+
+  // Lewitt Artwork with pure JS
+  // const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  // svg.setAttribute("width", svgWidth);
+  // svg.setAttribute("height", svgHeight);
+  // document.body.appendChild(svg);
+
+  // const mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
+  // mask.setAttribute("id", "circle-mask");
+  // svg.appendChild(mask);
+
+  // const rectMask = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  // rectMask.setAttribute("width", svgWidth);
+  // rectMask.setAttribute("height", svgHeight);
+  // rectMask.setAttribute("fill", "black");
+  // mask.appendChild(rectMask);
+
+  // const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  // circle.setAttribute("cx", svgWidth / 6);
+  // circle.setAttribute("cy", svgHeight / 4);
+  // circle.setAttribute("r", 50);
+  // circle.setAttribute("fill", "white");
+  // mask.appendChild(circle);
+
+  // const mask2 = document.createElementNS("http://www.w3.org/2000/svg", "mask");
+  // mask2.setAttribute("id", "circle-mask2");
+  // svg.appendChild(mask2);
+
+  // const rectMask2 = document.createElementNS(
+  //   "http://www.w3.org/2000/svg",
+  //   "rect"
+  // );
+  // rectMask2.setAttribute("width", svgWidth);
+  // rectMask2.setAttribute("height", svgHeight);
+  // rectMask2.setAttribute("fill", "white");
+  // mask2.appendChild(rectMask2);
+
+  // const circle2 = document.createElementNS(
+  //   "http://www.w3.org/2000/svg",
+  //   "circle"
+  // );
+  // circle2.setAttribute("cx", svgWidth / 6);
+  // circle2.setAttribute("cy", svgHeight / 4);
+  // circle2.setAttribute("r", 50);
+  // circle2.setAttribute("fill", "black");
+  // mask2.appendChild(circle2);
+
+  // for (let i = 0; i < 25; i++) {
+  //   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  //   rect.setAttribute("y", i * 20);
+  //   rect.setAttribute("width", svgWidth);
+  //   rect.setAttribute("height", 10);
+  //   rect.setAttribute("mask", "url(#circle-mask)");
+  //   svg.appendChild(rect);
+  // }
+
+  // for (let i = 0; i < 50; i++) {
+  //   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  //   rect.setAttribute("x", i * 20);
+  //   rect.setAttribute("width", 10);
+  //   rect.setAttribute("height", svgHeight);
+  //   rect.setAttribute("mask", "url(#circle-mask2)");
+  //   svg.appendChild(rect);
+  // }
+
   return (
     <div className="App">
       <h1>Hello SVG</h1>
@@ -57,6 +148,60 @@ export default function Fundamentals() {
         <text x="40" y="340" fill="black">
           JKL
         </text>
+      </svg> */}
+      {/* Line Chart example from Figma
+      <svg width="608" height="408" fill="none">
+        <g filter="url(#filter0_d_0_3)">
+          <rect
+            width="600"
+            height="400"
+            transform="translate(4)"
+            fill="white"
+          />
+          <path
+            d="M66 372L155.405 155.201L225 304L286 212L359.5 372L436 63.5L496 372"
+            stroke="black"
+            stroke-width="20"
+            stroke-linejoin="round"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_d_0_3"
+            x="0"
+            y="0"
+            width="608"
+            height="408"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_0_3"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_0_3"
+              result="shape"
+            />
+          </filter>
+        </defs>
       </svg> */}
       <svg width="500" height={svgHeight}>
         <rect
