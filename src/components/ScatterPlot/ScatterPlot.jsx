879b642ff9b848faf3790a1a298b28cbfce7ebb6
StefanIulianGrigoriu <grigoriu.stefan.iulian@gmail.com>
2023-03-14 14:48:00 +0000

    add row parser and csv data

--- src/components/ScatterPlot/ScatterPlot.jsx
+++ src/components/ScatterPlot/ScatterPlot.jsx
@@ -1,14 +1,21 @@
-import { select } from "d3";
-import { useEffect } from "react";
+import { select, csv } from "d3";
+import { useEffect, useState } from "react";
 
-import { svgWidth, svgHeight } from "./utils";
+import { svgWidth, svgHeight, csvUrl, parseRow } from "./utils";
 
 export const ScatterPlot = () => {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
+    csv(csvUrl, parseRow).then((d) => {
+      setData(d);
+    });
+    console.log("data", data);
+
     const svg = select("#scatter-plot")
       .attr("width", svgWidth)
       .attr("height", svgHeight);
-  });
+  }, []);
 
   return <svg id="scatter-plot" />;
 };
--- src/components/ScatterPlot/utils.js
+++ src/components/ScatterPlot/utils.js
@@ -1,2 +1,17 @@
 export const svgWidth = window.innerWidth;
 export const svgHeight = window.innerHeight;
+export const csvUrl = [
+  "https://gist.githubusercontent.com/",
+  "curran/",
+  "a08a1080b88344b0c8a7/",
+  "raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/",
+  "iris.csv",
+].join("");
+
+export const parseRow = (d) => ({
+  sepalLength: +d.sepal_length,
+  sepalWidth: +d.sepal_width,
+  petalLength: +d.petal_length,
+  petalWidth: +d.petal_width,
+  species: d.species,
+});
