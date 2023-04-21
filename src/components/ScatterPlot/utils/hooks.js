3ef63d11d3bb9698687b6a39a099d6f44ffc368f
StefanIulianGrigoriu <grigoriu.stefan.iulian@gmail.com>
2023-04-21 12:55:10 +0000

    refactor useScatterPlotData

--- src/components/ScatterPlot/utils/hooks.js
+++ src/components/ScatterPlot/utils/hooks.js
@@ -1,19 +1,15 @@
 import { useEffect, useState } from "react";
-
 import { getMainData } from "./index";
 
 export const useScatterPlotData = () => {
   const [plotData, setPlotData] = useState({
-    data: [],
     isLoading: true,
+    data: [],
   });
 
   useEffect(() => {
     getMainData(setPlotData);
   }, []);
 
-  return {
-    data: plotData.data,
-    isLoading: plotData.isLoading,
-  };
+  return plotData;
 };
